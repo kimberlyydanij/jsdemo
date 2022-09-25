83707fa2792f4f347afbb49aee62d4b71044be88
kimberlyydanij <kimberlydanij@gmail.com>
2022-09-26 00:25:34 +0900

    20220926 | Update

--- js033_quiz.js
+++ js033_quiz.js
@@ -38,55 +38,26 @@ sumAvg(newArr);
   [ '이영희', 100, 35, 75, 210, '70.00' ]]
   최고점: 71.67
 */
-console.log("문제 2: 2차원 배열의 데이터를 이용해서 합계와 평균을 계산")
-
-/*
-let rawData = [['홍길동', 90, 85, 40], ['이영희', 100, 35, 75]];
-let sum1=0;
-let lt = 0;
-
-function jumsu(data) {
-    for(j=0;j<data.length;j++) {
-       for(i=0;i<data[j].length;i++) {
-        sum1 = sum1 + data[j][i+1]; }
-        lt = data[j].length;
-        console.log(lt);
-        data[j][lt] = sum1;
-        console.log(data[j]);
-        }
-       }  
-
-/*
-function jumsu1(a)  {      
-        for(i=1;i<a.length;i++) {
-            console.log(a.length)
-         } }  
-jumsu([['홍길동', 90, 85, 40], ['이영희', 100, 35, 75]]);
-[출력결과]
- [[ '홍길동', 90, 85, 40, 215, '71.67' ],
-  [ '이영희', 100, 35, 75, 210, '70.00' ]]
-  최고점: 71.67  */
 
 console.log("문제 2: 2차원 배열의 데이터를 이용해서 합계와 평균을 계산")
 
-let sum1=0;
-let ave1=0;
-let lt=0;
 function jumsu(data) {
-  sSum(data);
-}
-function sSum(data) {
+  let max = [];
   for(i=0;i<data.length;i++) {
-    console.log(i);
-    for(j=1;j<=data[i].length+1;j++) {
+    let sum1=0;
+    let lt=0;
+    for(j=1;j<data[i].length;j++) {
       sum1 = sum1 + data[i][j];
-      parseInt(sum1);
-      console.log(sum1);
+      lt = data[i].length;
       }
-    lt = data[i].length;
-    data[i][lt] = sum1;
-    console.log(data[i]);
-  } 
-}
-jumsu([['홍길동', 90, 85, 40], ['이영희', 100, 35, 75]]);
+      lt = lt-1;
+      data[i].push(sum1);
+      sum1 = sum1/lt;
+      sum1 = sum1.toFixed(2);
+      data[i].push(sum1);
+      console.log(data[i]);
+      max[i] = sum1;
+      }
+      console.log(`최고점: ${ Math.max.apply(null,max)}`);
+} 
 jumsu([['홍길동', 90, 85, 40], ['이영희', 100, 35, 75]]);
